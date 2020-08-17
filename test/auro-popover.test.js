0d039519a4c66fccf64454f9500205ff8c1e26e0
Gus Naughton <gus.naughton@alaskaair.com>
2020-08-17 11:22:19 -0700

    test: fix tests

--- test/auro-popover.test.js
+++ test/auro-popover.test.js
@@ -3,10 +3,19 @@ import '../src/auro-popover.js';
 
 
 describe('auro-popover', () => {
-  it('auro-popover is accessible', async () => {
+  it('auro-popover is accessible from the bottom', async () => {
     const el = await fixture(html`
       <button id="button1">Test</button>
       <auro-popover for="button1" placement="bottom"></auro-popover>
+    `);
+
+    await expect(el).to.be.accessible();
+  });
+
+  it('auro-popover is accessible from the top', async () => {
+    const el = await fixture(html`
+      <button id="button1">Test</button>
+      <auro-popover for="button1" placement="top"></auro-popover>
     `);
 
     await expect(el).to.be.accessible();
@@ -16,41 +25,5 @@ describe('auro-popover', () => {
     const el = await Boolean(customElements.get("auro-popover"));
 
     await expect(el).to.be.true;
-  });
-
-  it('auro-popover for works correctly', async () => {
-    const el = await fixture(html`
-    <div id="container">
-      <button id="button4">Test</button>
-      <auro-popover for="button4" placement="top"></auro-popover>
-    </div>
-    `);
-
-    el.querySelector("#button4").dispatchEvent(new MouseEvent('focus'), {
-      view: window,
-      bubbles: true,
-      cancelable: true
-    });
-    const div = el.querySelector("auro-popover").shadowRoot.querySelector('#tooltip');
-    debugger;
-    expect(true).to.equal(true);
-  });
-
-  it('auro-popover placement works correctly', async () => {
-    const el = await fixture(html`
-    <div id="container">
-      <button id="button5">Test</button>
-      <auro-popover for="button5" placement="bottom"></auro-popover>
-    </div>
-    `);
-
-    el.querySelector("#button5").dispatchEvent(new MouseEvent('mouseenter'), {
-      view: window,
-      bubbles: true,
-      cancelable: true
-    });
-    const div = el.querySelector("auro-popover").shadowRoot.querySelector('#tooltip');
-
-    expect(true).to.equal(true);
   });
 });
