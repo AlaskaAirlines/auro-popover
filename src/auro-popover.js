d62aead2e43ead51603ae826a841a0e3daa3a4c1
Gus Naughton <gus.naughton@alaskaair.com>
2020-08-17 15:30:05 -0700

    feat: change role to alert

--- src/auro-popover.js
+++ src/auro-popover.js
@@ -95,7 +95,7 @@ class AuroPopover extends LitElement {
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
-      <div id="popover" class="popover util_insetLg" role="popover">
+      <div id="popover" class="popover util_insetLg" role="alert">
         <slot></slot>
         <div id="arrow" class="arrow" data-popper-arrow></div>
       </div>
