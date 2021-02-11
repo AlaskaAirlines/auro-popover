424a68c5fe81159bc34c8c60b4bd2804140b7d3d
Geoff Rich <geoff.rich@alaskaair.com>
2021-02-11 15:38:37 -0800

    style: lint fixes and consistent tabbing

--- src/popover.js
+++ src/popover.js
@@ -7,45 +7,47 @@ import { createPopper } from '@popperjs/core/dist/esm/popper';
 
 // build the component class
 const POPOVER_OFFSET_MAX = 18,
- POPOVER_OFFSET_MIN = 0;
+  POPOVER_OFFSET_MIN = 0;
 
 export default class Popover {
 
-    constructor(anchor, popover, placement) {
-        this.anchor = anchor;
-        this.popover = popover;
-        this.options = {placement,
-            visibleClass: 'data-show'};
-        this.popover.classList.remove(this.options.visibleClass);
+  constructor(anchor, popover, placement) {
+    this.anchor = anchor;
+    this.popover = popover;
+    this.options = {
+      placement,
+      visibleClass: 'data-show'
+    };
+    this.popover.classList.remove(this.options.visibleClass);
+  }
+
+  show() {
+    if (this.popper) {
+      this.popper.destroy();
     }
 
-    show() {
-        if (this.popper) {
-            this.popper.destroy();
+    this.popper = createPopper(this.anchor, this.popover, {
+      tooltip: this.anchor,
+      placement: this.options.placement,
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [
+              POPOVER_OFFSET_MIN,
+              POPOVER_OFFSET_MAX
+            ]
+          }
         }
+      ]
+    })
+  }
 
-        this.popper = createPopper(this.anchor, this.popover, {
-            tooltip: this.anchor,
-            placement: this.options.placement,
-            modifiers: [
-                {
-                name: 'offset',
-                options: {
-                    offset: [
-                        POPOVER_OFFSET_MIN,
-                        POPOVER_OFFSET_MAX
-                    ]
-                }
-            }
-        ]
-        })
-    }
+  triggerUpdate() {
+    this.popper.update();
+  }
 
-    triggerUpdate() {
-        this.popper.update();
-    }
-
-    hide() {
-        this.popover.classList.remove(this.options.visibleClass);
-    }
+  hide() {
+    this.popover.classList.remove(this.options.visibleClass);
+  }
 }
