4cc8722939e1ab7b3a20f930308f9fa6172480f7
Jordan Jones <jmjones2463@gmail.com>
2024-03-07 18:10:30 -0800

    feat(examples): implement example js system

---
+++ demo/api.js
@@ -0,0 +1,19 @@
+/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
+
+import { boundaryExample } from "../apiExamples/boundary";
+
+export function initExamples(initCount) {
+  initCount = initCount || 0;
+
+  try {
+    // javascript example function calls to be added here upon creation to test examples
+    boundaryExample();
+  } catch (err) {
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
