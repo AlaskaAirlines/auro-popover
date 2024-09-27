d080c609faec0c6bd990204f5d72ef5c35c4ad8b
Jordan Jones <jmjones2463@gmail.com>
2024-09-27 13:18:46 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroPopover } from './src/auro-popover.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
- const registerComponent = (name = 'custom-popover') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroPopover {});
-  }
-}
-
-export { registerComponent }
+RuntimeUtils.default.prototype.registerComponent('custom-popover', AuroPopover);
